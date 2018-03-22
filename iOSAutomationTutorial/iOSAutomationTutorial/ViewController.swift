b4a82a2791e5b9297963a32b783f65e5c31a6df7
Mohammad Alatrash <mkalatrash@gmail.com>
2018-03-22 08:52:37 +0400

    initial commit

---
+++ iOSAutomationTutorial/iOSAutomationTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSAutomationTutorial
+//
+//  Created by Muhammad Alatrash on 3/22/18.
+//  Copyright © 2018 MKApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
